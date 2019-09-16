2e0c4bb503797958062282b90c743ac95a387950
SWH Academy <49841302+swhacademy@users.noreply.github.com>
2019-09-16 18:39:59 +0900

    Update embed.js

--- embed.js
+++ embed.js
@@ -18,8 +18,8 @@
   iframe.setAttribute('allowfullscreen', true);
   iframe.setAttribute('allowtransparency', true);
   iframe.src = script.src.replace(/^http:/, 'https:').replace(/embed\.js/, 'embed.html');
-  iframe.width = hasUI ? 482 : 480;
-  iframe.height = hasUI ? 393 : 360;
+  iframe.width = hasUI ? 482 : 700;
+  iframe.height = hasUI ? 393 : 600;
   iframe.style.border = '0';
   iframe.className = 'phosphorus';
 
